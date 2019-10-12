dcea051d02198dfd8b60f559ad7db9e38b43f8ac
vladji <vladjislav@gmail.com>
2019-10-12 13:49:04 +0300

    feat: js-edu is done:)

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family',
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  const val = config[focus];
+  const weeks = (knowsProgramming) ? 800 / val : 1300 / val;
+  return Math.ceil(weeks);
+};
